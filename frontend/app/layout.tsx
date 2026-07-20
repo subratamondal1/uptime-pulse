a1e6a8aa4b3e47030dcde3067bb5488520f00f9a
Subrata Mondal <subratasubha2@gmail.com>
2026-07-20 16:53:35 +0530

    fix(frontend): default theme to light instead of system

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -30,7 +30,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false}
+        >
           {children}
         </ThemeProvider>
       </body>
